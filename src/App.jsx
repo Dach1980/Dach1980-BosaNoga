accbbc922e52fdfeacb41c3be8a0b5430df1f276
Dmitry Cherkashin <dchpost@mail.ru>
2024-12-04 11:49:10 +1000

    создает отдельную папку для html

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux'
--- src/selectors/index.jsx
+++ src/selectors/index.jsx
@@ -1,5 +1,9 @@
 export const categoriesListSelector = (state) => state.categoriesList;
-export const productsListSelector = (state) => state.productsList;
+export const productsListSelector = (state) => ({
+    items: state.productsList.items || [],
+    loading: state.productsList.loading,
+    error: state.productsList.error
+});
 export const bestsellersListSelector = (state) => state.bestsellersList;
 export const cartItemsSelector = (state) => state.cartItems;
 export const cartFormSelector = (state) => state.cartForm;
